7aa632136c758a51f8089ffe62d42361a008dfd4
SujalPandya04 <23bsit055@charusat.edu.in>
2026-03-25 12:04:12 +0530

    Create index.js for component exports in Slowgram project

---
+++ client/src/components/index.js
@@ -0,0 +1,8 @@
+// Exports for Slowgram components
+
+export { default as Header } from './Header';
+export { default as Footer } from './Footer';
+export { default as Post } from './Post';
+export { default as Sidebar } from './Sidebar';
+export { default as Profile } from './Profile';
+export { default as Feed } from './Feed';
