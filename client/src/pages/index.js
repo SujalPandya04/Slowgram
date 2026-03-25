214bd84a4822f693e46a9f6ea08d1d33fb1ab386
SujalPandya04 <23bsit055@charusat.edu.in>
2026-03-25 12:04:28 +0530

    Create index.js file for pages exports

---
+++ client/src/pages/index.js
@@ -0,0 +1,9 @@
+// Pages index file for Slowgram
+
+export { default as Home } from './Home';
+export { default as Profile } from './Profile';
+export { default as Explore } from './Explore';
+export { default as Messages } from './Messages';
+export { default as Notifications } from './Notifications';
+export { default as Login } from './Login';
+export { default as Register } from './Register';
